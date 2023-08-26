236e8ebdafa7d74312036ddf8e1c64e62e3271d1
Shlomi Nugarker <shlomin1231@gmail.com>
2023-08-26 12:38:50 +0300

    registrationForm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './cmps/app-root/app.component';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, SignInComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/pages/sign-in/sign-in.component.ts
+++ src/app/pages/sign-in/sign-in.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.scss']
+  styleUrls: ['./sign-in.component.scss'],
 })
-export class SignInComponent {
+export class SignInComponent implements OnInit {
+  constructor(private fb: FormBuilder) {}
+  registrationForm!: FormGroup;
+  capturedImage: string | null = null;
 
+  ngOnInit(): void {
+    this.registrationForm = this.fb.group({
+      fullName: ['', Validators.required],
+      language: ['', Validators.required],
+      phone: ['', Validators.required],
+      customerFace: [null, Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.registrationForm.valid) {
+      console.log('Form submitted:', this.registrationForm.value);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+
+  captureImage() {
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
+      .then((stream) => {
+        const video = document.createElement('video');
+        video.srcObject = stream;
+        video.onloadedmetadata = () => {
+          video.play();
+          const canvas = document.createElement('canvas');
+          canvas.width = video.videoWidth;
+          canvas.height = video.videoHeight;
+
+          const context = canvas.getContext('2d');
+          context?.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+          this.capturedImage = canvas.toDataURL('image/png');
+
+          this.registrationForm.patchValue({
+            customerFace: this.capturedImage,
+          });
+
+          video.srcObject = null;
+          stream.getTracks().forEach((track) => track.stop());
+        };
+      })
+      .catch((err) => {
+        console.error('Error accessing camera:', err);
+      });
+  }
 }
