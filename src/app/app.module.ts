414245865b934a49fb66a73695f30a3a55e98edf
Shlomi Nugarker <shlomin1231@gmail.com>
2023-08-26 11:15:44 +0300

    add changes

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './cmps/app-root/app.component';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
+
+@NgModule({
+  declarations: [AppComponent, SignInComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
